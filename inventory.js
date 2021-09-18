9cec0011ac6c13bc67de84f80a6aca49c6c97a2d
Nate Tyalor <taylonr@gmail.com>
2021-09-18 09:01:55 -0500

    Starting point

--- inventory.js
+++ inventory.js
@@ -1,24 +1,6 @@
 const inventory = require("./inventory.json");
 
-const updateInventory = () => {
-  const updates = require("./inventory_update.json");
-  for (let i = 0; i < updates.length; i++) {
-    const update = updates[i];
-
-    for (let j = 0; j < inventory.length; j++) {
-      const item = inventory[j];
-      if (update.id === item.id) {
-        delete update.id;
-        const key = Object.keys(update)[0];
-
-        item[key] = update[key];
-        break;
-      }
-    }
-  }
-
-  return inventory;
-};
+const updateInventory = () => {};
 
 module.exports = {
   updateInventory,
