4bdac558cda0d27169997bffb7722f110ef5a8ae
Nate Tyalor <taylonr@gmail.com>
2021-09-18 14:05:12 -0500

    Wrapping up challenge 5

--- inventory.js
+++ inventory.js
@@ -1,61 +1,20 @@
 const fs = require("fs");
 
-const getInventoryObject = () => {
-  const inventory = require("./inventory.json");
-  //   const result = {
-  //     boot: inventory
-  //       .filter((i) => i.itemType === "boot")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     tents: inventory
-  //       .filter((i) => i.itemType === "tents")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     kayak: inventory
-  //       .filter((i) => i.itemType === "kayak")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     sleepingBags: inventory
-  //       .filter((i) => i.itemType === "sleepingBags")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     shirts: inventory
-  //       .filter((i) => i.itemType === "shirts")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     backpacks: inventory
-  //       .filter((i) => i.itemType === "backpacks")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //   };
+const calculateRevenue = () => {
+  const transactions = require("./transactions.json");
 
-  //   return result;
+  return transactions
+    .filter((t) => {
+      const tDate = new Date(t.transactionDate);
+      return tDate.getFullYear() === 2021 && tDate.getUTCMonth() === 2;
+    })
+    .reduce((rev, t) => {
+      rev += t.amount;
 
-  return inventory.reduce((obj, i) => {
-    const itemType = i.itemType;
-
-    if (!obj[itemType]) {
-      obj[itemType] = [];
-    }
-
-    delete i.itemType;
-    obj[itemType].push(i);
-    return obj;
-  }, {});
+      return rev;
+    }, 0);
 };
 
 module.exports = {
-  getInventoryObject,
+  calculateRevenue,
 };
--- tests/challenge.test.js
+++ tests/challenge.test.js
@@ -1,147 +1,25 @@
 const { assert } = require("chai");
 const fs = require("fs");
 
-const { getInventoryObject } = require("../inventory");
+const { calculateRevenue } = require("../inventory");
 
-describe("Converting to an object", () => {
-  let isObject = false;
-  const missingKeys = [];
-  let valuesAreArray = false;
-  let noItemTypes = false;
-  let onlyValidProperties = false;
+describe("Calculating revenue", () => {
+  let isNumber = false;
 
-  const result = getInventoryObject();
+  const result = calculateRevenue();
 
-  it("Should return an object", () => {
-    isObject =
-      typeof result === "object" && result !== null && !Array.isArray(result);
+  it("Should return a number", () => {
+    isNumber = Number(result) === result;
 
-    assert(isObject, "Make sure you return an object");
+    assert(isNumber, "Make sure to return a number from the function.");
   });
 
-  it("Should have a key for each type", () => {
-    if (!isObject) return;
-
-    if (!result.hasOwnProperty("boot")) {
-      missingKeys.push("boot");
-    }
-
-    if (!result.hasOwnProperty("kayak")) {
-      missingKeys.push("kayak");
-    }
-
-    if (!result.hasOwnProperty("sleepingBags")) {
-      missingKeys.push("sleepingBags");
-    }
-
-    if (!result.hasOwnProperty("tents")) {
-      missingKeys.push("tents");
-    }
-
-    if (!result.hasOwnProperty("shirts")) {
-      missingKeys.push("shirts");
-    }
-
-    if (!result.hasOwnProperty("backpacks")) {
-      missingKeys.push("backpacks");
-    }
+  it("Should return the correct amount", () => {
+    if (!isNumber) return;
 
     assert(
-      missingKeys.length === 0,
-      `The following item types were missing ${missingKeys.join(",")}`
-    );
-  });
-
-  it("Should return arrays for each itemType", () => {
-    if (missingKeys.length) return;
-
-    const values = Object.values(result);
-
-    valuesAreArray = values.every((v) => Array.isArray(v));
-
-    assert(valuesAreArray, "The value for each item type should be an array");
-  });
-
-  it("Should not have any item type properties", () => {
-    if (!valuesAreArray) return;
-
-    const values = Object.values(result);
-
-    noItemTypes = values.every((val) => {
-      return val.every((v) => !v.hasOwnProperty("itemType"));
-    });
-
-    assert(noItemTypes, "Objects should not have an itemType property");
-  });
-
-  it("Should include all valid properties", () => {
-    if (!noItemTypes) return;
-
-    const values = Object.values(result);
-
-    onlyValidProperties = values.every((val) => {
-      return val.every(
-        (v) =>
-          v.hasOwnProperty("id") &&
-          v.hasOwnProperty("brand") &&
-          v.hasOwnProperty("color") &&
-          v.hasOwnProperty("price") &&
-          v.hasOwnProperty("cost")
-      );
-    });
-
-    assert(
-      onlyValidProperties,
-      "Objects should have the keys id, brand, color, price, cost"
-    );
-  });
-
-  it("should have the correct objects for each item type", () => {
-    if (!onlyValidProperties) return;
-
-    const expected = require("./results.json");
-
-    const getIds = (_) => _.id;
-    const areEqual = (a, b) =>
-      a.length == b.length && a.every((v, i) => b.includes(v));
-
-    const actual_boots = result.boot.map(getIds);
-    const actual_kayaks = result.kayak.map(getIds);
-    const actual_bags = result.sleepingBags.map(getIds);
-    const actual_tents = result.tents.map(getIds);
-    const actual_shirts = result.shirts.map(getIds);
-    const actual_backpacks = result.backpacks.map(getIds);
-
-    const bootIds = expected.boot.map(getIds);
-    const kayakIds = expected.kayak.map(getIds);
-    const bags = expected.sleepingBags.map(getIds);
-    const tents = expected.tents.map(getIds);
-    const shirts = expected.shirts.map(getIds);
-    const backpacks = expected.backpacks.map(getIds);
-
-    assert(
-      areEqual(actual_boots, bootIds),
-      `The boot property should have the Ids: ${bootIds}`
-    );
-    assert(
-      areEqual(actual_kayaks, kayakIds),
-      `The kayaks property should have the Ids: ${kayakIds}`
-    );
-    assert(
-      areEqual(actual_bags, bags),
-      `The sleepingBags property should have the Ids: ${bags}`
-    );
-    assert(
-      areEqual(actual_tents, tents),
-      `The tents property should have the Ids: ${tents}`
-    );
-    assert(
-      areEqual(actual_shirts, shirts),
-      `The shirts property should have the Ids: ${shirts}`
-    );
-    assert(
-      areEqual(actual_backpacks, backpacks),
-      `The backpacks property should have the Ids: ${backpacks}`
+      result === 1439.39,
+      `Only add the ammounts for transactions that occurred in 2021-03. Expected 1439.39 but received ${result}`
     );
   });
 });
