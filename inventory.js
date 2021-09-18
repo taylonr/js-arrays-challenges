edcfef37d314979cf9bdab38d963ebc120ab6e07
Nate Tyalor <taylonr@gmail.com>
2021-09-18 13:08:06 -0500

    Wrapping up challenge 3 test

--- inventory.js
+++ inventory.js
@@ -1,61 +1,10 @@
 const fs = require("fs");
 
-const getInventoryObject = () => {
+const getBoots = () => {
   const inventory = require("./inventory.json");
-  //   const result = {
-  //     boot: inventory
-  //       .filter((i) => i.itemType === "boot")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     tents: inventory
-  //       .filter((i) => i.itemType === "tents")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     kayak: inventory
-  //       .filter((i) => i.itemType === "kayak")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     sleepingBags: inventory
-  //       .filter((i) => i.itemType === "sleepingBags")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     shirts: inventory
-  //       .filter((i) => i.itemType === "shirts")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //     backpacks: inventory
-  //       .filter((i) => i.itemType === "backpacks")
-  //       .map((i) => {
-  //         delete i.itemType;
-  //         return i;
-  //       }),
-  //   };
-
-  //   return result;
-
-  return inventory.reduce((obj, i) => {
-    const itemType = i.itemType;
-
-    if (!obj[itemType]) {
-      obj[itemType] = [];
-    }
-
-    delete i.itemType;
-    obj[itemType].push(i);
-    return obj;
-  }, {});
+  return inventory.filter((_) => _.itemType === "boot");
 };
 
 module.exports = {
-  getInventoryObject,
+  getBoots,
 };
--- tests/challenge.test.js
+++ tests/challenge.test.js
@@ -1,147 +1,205 @@
 const { assert } = require("chai");
 const fs = require("fs");
 
-const { getInventoryObject } = require("../inventory");
+const { getBoots } = require("../inventory");
 
-describe("Converting to an object", () => {
-  let isObject = false;
-  const missingKeys = [];
-  let valuesAreArray = false;
-  let noItemTypes = false;
-  let onlyValidProperties = false;
+describe("Getting a sublist", () => {
+  let isArray = false;
+  let bootLength = 0;
+  let allAreBoots = false;
 
-  const result = getInventoryObject();
+  it("Should return an array", () => {
+    const result = getBoots();
 
-  it("Should return an object", () => {
-    isObject =
-      typeof result === "object" && result !== null && !Array.isArray(result);
+    isArray = Array.isArray(result);
 
-    assert(isObject, "Make sure you return an object");
+    assert(isArray, "Make sure to return an array");
   });
 
-  it("Should have a key for each type", () => {
-    if (!isObject) return;
+  it("Should only be 18 items", () => {
+    if (!isArray) return;
 
-    if (!result.hasOwnProperty("boot")) {
-      missingKeys.push("boot");
-    }
+    const result = getBoots();
 
-    if (!result.hasOwnProperty("kayak")) {
-      missingKeys.push("kayak");
-    }
-
-    if (!result.hasOwnProperty("sleepingBags")) {
-      missingKeys.push("sleepingBags");
-    }
-
-    if (!result.hasOwnProperty("tents")) {
-      missingKeys.push("tents");
-    }
-
-    if (!result.hasOwnProperty("shirts")) {
-      missingKeys.push("shirts");
-    }
-
-    if (!result.hasOwnProperty("backpacks")) {
-      missingKeys.push("backpacks");
-    }
+    bootLength = result.length;
 
     assert(
-      missingKeys.length === 0,
-      `The following item types were missing ${missingKeys.join(",")}`
+      bootLength === 18,
+      `Make sure to return all boots, received ${bootLength} but expected 18`
     );
   });
 
-  it("Should return arrays for each itemType", () => {
-    if (missingKeys.length) return;
+  it("Should have every item be a boot", () => {
+    if (bootLength !== 18) return;
 
-    const values = Object.values(result);
+    const result = getBoots();
 
-    valuesAreArray = values.every((v) => Array.isArray(v));
+    allAreBoots = result.every((r) => r.itemType === "boot");
 
-    assert(valuesAreArray, "The value for each item type should be an array");
+    assert(allAreBoots, "Make sure to only include boots");
   });
 
-  it("Should not have any item type properties", () => {
-    if (!valuesAreArray) return;
+  it("Should include the full object", () => {
+    if (!allAreBoots) return;
 
-    const values = Object.values(result);
+    const result = getBoots();
 
-    noItemTypes = values.every((val) => {
-      return val.every((v) => !v.hasOwnProperty("itemType"));
-    });
-
-    assert(noItemTypes, "Objects should not have an itemType property");
-  });
-
-  it("Should include all valid properties", () => {
-    if (!noItemTypes) return;
-
-    const values = Object.values(result);
-
-    onlyValidProperties = values.every((val) => {
-      return val.every(
-        (v) =>
-          v.hasOwnProperty("id") &&
-          v.hasOwnProperty("brand") &&
-          v.hasOwnProperty("color") &&
-          v.hasOwnProperty("price") &&
-          v.hasOwnProperty("cost")
-      );
-    });
-
-    assert(
-      onlyValidProperties,
-      "Objects should have the keys id, brand, color, price, cost"
+    const allProps = result.every(
+      (r) =>
+        r.hasOwnProperty("id") &&
+        r.hasOwnProperty("itemType") &&
+        r.hasOwnProperty("brand") &&
+        r.hasOwnProperty("color") &&
+        r.hasOwnProperty("price") &&
+        r.hasOwnProperty("cost")
     );
-  });
-
-  it("should have the correct objects for each item type", () => {
-    if (!onlyValidProperties) return;
-
-    const expected = require("./results.json");
-
-    const getIds = (_) => _.id;
-    const areEqual = (a, b) =>
-      a.length == b.length && a.every((v, i) => b.includes(v));
-
-    const actual_boots = result.boot.map(getIds);
-    const actual_kayaks = result.kayak.map(getIds);
-    const actual_bags = result.sleepingBags.map(getIds);
-    const actual_tents = result.tents.map(getIds);
-    const actual_shirts = result.shirts.map(getIds);
-    const actual_backpacks = result.backpacks.map(getIds);
-
-    const bootIds = expected.boot.map(getIds);
-    const kayakIds = expected.kayak.map(getIds);
-    const bags = expected.sleepingBags.map(getIds);
-    const tents = expected.tents.map(getIds);
-    const shirts = expected.shirts.map(getIds);
-    const backpacks = expected.backpacks.map(getIds);
 
     assert(
-      areEqual(actual_boots, bootIds),
-      `The boot property should have the Ids: ${bootIds}`
-    );
-    assert(
-      areEqual(actual_kayaks, kayakIds),
-      `The kayaks property should have the Ids: ${kayakIds}`
-    );
-    assert(
-      areEqual(actual_bags, bags),
-      `The sleepingBags property should have the Ids: ${bags}`
-    );
-    assert(
-      areEqual(actual_tents, tents),
-      `The tents property should have the Ids: ${tents}`
-    );
-    assert(
-      areEqual(actual_shirts, shirts),
-      `The shirts property should have the Ids: ${shirts}`
-    );
-    assert(
-      areEqual(actual_backpacks, backpacks),
-      `The backpacks property should have the Ids: ${backpacks}`
+      allProps,
+      "Each object should have the fields: id, itemType, brand, color, price, cost"
     );
   });
 });
+
+// describe("Converting to an object", () => {
+//   let isObject = false;
+//   const missingKeys = [];
+//   let valuesAreArray = false;
+//   let noItemTypes = false;
+//   let onlyValidProperties = false;
+
+//   const result = getInventoryObject();
+
+//   it("Should return an object", () => {
+//     isObject =
+//       typeof result === "object" && result !== null && !Array.isArray(result);
+
+//     assert(isObject, "Make sure you return an object");
+//   });
+
+//   it("Should have a key for each type", () => {
+//     if (!isObject) return;
+
+//     if (!result.hasOwnProperty("boot")) {
+//       missingKeys.push("boot");
+//     }
+
+//     if (!result.hasOwnProperty("kayak")) {
+//       missingKeys.push("kayak");
+//     }
+
+//     if (!result.hasOwnProperty("sleepingBags")) {
+//       missingKeys.push("sleepingBags");
+//     }
+
+//     if (!result.hasOwnProperty("tents")) {
+//       missingKeys.push("tents");
+//     }
+
+//     if (!result.hasOwnProperty("shirts")) {
+//       missingKeys.push("shirts");
+//     }
+
+//     if (!result.hasOwnProperty("backpacks")) {
+//       missingKeys.push("backpacks");
+//     }
+
+//     assert(
+//       missingKeys.length === 0,
+//       `The following item types were missing ${missingKeys.join(",")}`
+//     );
+//   });
+
+//   it("Should return arrays for each itemType", () => {
+//     if (missingKeys.length) return;
+
+//     const values = Object.values(result);
+
+//     valuesAreArray = values.every((v) => Array.isArray(v));
+
+//     assert(valuesAreArray, "The value for each item type should be an array");
+//   });
+
+//   it("Should not have any item type properties", () => {
+//     if (!valuesAreArray) return;
+
+//     const values = Object.values(result);
+
+//     noItemTypes = values.every((val) => {
+//       return val.every((v) => !v.hasOwnProperty("itemType"));
+//     });
+
+//     assert(noItemTypes, "Objects should not have an itemType property");
+//   });
+
+//   it("Should include all valid properties", () => {
+//     if (!noItemTypes) return;
+
+//     const values = Object.values(result);
+
+//     onlyValidProperties = values.every((val) => {
+//       return val.every(
+//         (v) =>
+//           v.hasOwnProperty("id") &&
+//           v.hasOwnProperty("brand") &&
+//           v.hasOwnProperty("color") &&
+//           v.hasOwnProperty("price") &&
+//           v.hasOwnProperty("cost")
+//       );
+//     });
+
+//     assert(
+//       onlyValidProperties,
+//       "Objects should have the keys id, brand, color, price, cost"
+//     );
+//   });
+
+//   it("should have the correct objects for each item type", () => {
+//     if (!onlyValidProperties) return;
+
+//     const expected = require("./results.json");
+
+//     const getIds = (_) => _.id;
+//     const areEqual = (a, b) =>
+//       a.length == b.length && a.every((v, i) => b.includes(v));
+
+//     const actual_boots = result.boot.map(getIds);
+//     const actual_kayaks = result.kayak.map(getIds);
+//     const actual_bags = result.sleepingBags.map(getIds);
+//     const actual_tents = result.tents.map(getIds);
+//     const actual_shirts = result.shirts.map(getIds);
+//     const actual_backpacks = result.backpacks.map(getIds);
+
+//     const bootIds = expected.boot.map(getIds);
+//     const kayakIds = expected.kayak.map(getIds);
+//     const bags = expected.sleepingBags.map(getIds);
+//     const tents = expected.tents.map(getIds);
+//     const shirts = expected.shirts.map(getIds);
+//     const backpacks = expected.backpacks.map(getIds);
+
+//     assert(
+//       areEqual(actual_boots, bootIds),
+//       `The boot property should have the Ids: ${bootIds}`
+//     );
+//     assert(
+//       areEqual(actual_kayaks, kayakIds),
+//       `The kayaks property should have the Ids: ${kayakIds}`
+//     );
+//     assert(
+//       areEqual(actual_bags, bags),
+//       `The sleepingBags property should have the Ids: ${bags}`
+//     );
+//     assert(
+//       areEqual(actual_tents, tents),
+//       `The tents property should have the Ids: ${tents}`
+//     );
+//     assert(
+//       areEqual(actual_shirts, shirts),
+//       `The shirts property should have the Ids: ${shirts}`
+//     );
+//     assert(
+//       areEqual(actual_backpacks, backpacks),
+//       `The backpacks property should have the Ids: ${backpacks}`
+//     );
+//   });
+// });
